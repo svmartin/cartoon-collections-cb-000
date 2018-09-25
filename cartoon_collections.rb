bc831a4a9a23e6be04b0820cb5b518653804146e
Sean Martin <sean.martin@gmail.com>
2018-09-25 00:06:47 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,22 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(collection)# code an argument here
   # Your code here
+  collection.each_with_index do |name, i|
+    puts "#{i+1} #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(collection)# code an argument here
   # Your code here
+  collection.map { |phrase| "#{phrase.capitalize}!" }
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(collection)# code an argument here
   # Your code here
+  collection.any? { |word| word.length > 4 }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(collection)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  collection.find { |str| cheese_types.include?(str) }
 end
