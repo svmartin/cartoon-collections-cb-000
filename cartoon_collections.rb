6bcf6e2847cc0fd04fb997ca662a683816816a4d
Sean Martin <sean.martin@gmail.com>
2018-09-25 00:05:21 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,8 @@ def long_planeteer_calls(collection)# code an argument here
   collection.any? { |word| word.length > 4 }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(collection)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  collection.find? { |str| cheese_types.include?(str) }
 end
