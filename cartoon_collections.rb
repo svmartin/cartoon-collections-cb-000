29158af91a28b0d412c8a7ccc56a7ce42939047b
Sean Martin <sean.martin@gmail.com>
2018-09-25 00:02:39 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,8 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(collection)# code an argument here
   # Your code here
+  collection.each_with_index do |name, i|
+    puts "#{i+1} #{name}"
+  end
 end
 
 def summon_captain_planet# code an argument here
